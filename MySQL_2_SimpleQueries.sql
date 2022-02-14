178c66c54abba76acd53afec9d9784b02fe24545
Joshua_Johnson <joshualj2018@gmail.com>
2022-02-14 16:38:28 -0600

    SecondSimpleQuery2: Banking

--- MySQL_2_SimpleQueries.sql
+++ MySQL_2_SimpleQueries.sql
@@ -12,14 +12,25 @@ select UNIQUE Title from employee;
 
 select e.LAST_NAME as `Last Name`, e.TITLE as `Title`, b.LAST_NAME as `boss's last name`, b.TITLE as `boss's title`
 from employee as e 
-join employee as b on e.SUPERIOR_EMP_ID = b.EMP_ID;
+left join employee as b on e.SUPERIOR_EMP_ID = b.EMP_ID;
 
-select a.PRODUCT_CD as `Product`, a.AVAIL_BALANCE as `Available Balance`, i.LAST_NAME as `Customer Last Name`
-from account as a
+select p.Name as `Product`, a.AVAIL_BALANCE as `Available Balance`, i.LAST_NAME as `Customer Last Name`
+from account as a 
+join product as p on p.PRODUCT_CD = a.PRODUCT_CD
 join individual as i on a.CUST_ID = i.CUST_ID;
 
 select a.*, i.LAST_NAME
 from acc_transaction as a
-join individual as i on a.ACCOUNT_ID = i.CUST_ID
+join account as t on a.ACCOUNT_ID = t.ACCOUNT_ID
+join individual as i on i.CUST_ID = t.CUST_ID
 where i.LAST_NAME like "T%";
+
+
+
+
+
+
+
+select * from acc_transaction;
+select * from individual;
 
